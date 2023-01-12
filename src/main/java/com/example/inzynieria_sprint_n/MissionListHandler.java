22b610008615f6e961b9acc9347919baee21e490
Mateusz Krzysiek <73441601+christaf@users.noreply.github.com>
2023-01-13 00:38:05 +0100

    MissionListHandler: drobne usprawnienia, dodawanie chwilowo do listy

--- src/main/java/com/example/inzynieria_sprint_n/MissionListHandler.java
+++ src/main/java/com/example/inzynieria_sprint_n/MissionListHandler.java
@@ -1,57 +1,48 @@
 package com.example.inzynieria_sprint_n;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MissionListHandler {
-    private File fileMissionList;
+    private final File fileMissionList;
+    private final List<Mission> missionList;
 
-    private List<Mission> missionList;
-
-    MissionListHandler(List <Mission> missionList) {
-        try {
-            this.missionList = missionList;
-            fileMissionList = new File("proposed_mission_list.csv");
+    MissionListHandler() throws IOException {
+        fileMissionList = new File("proposed_mission_list.csv");
+        if (!fileMissionList.exists()) {
             if (fileMissionList.createNewFile()) {
                 System.out.println("Plik stworzony prawidłowo");
-            } else {
-                System.out.println("Plik już istnieje!");
             }
-        } catch (IOException e) {
-            System.out.println("Blad tworzenia pliku!!!1!");
-            e.printStackTrace();
+        } else {
+            System.out.println("Plik już istnieje!");
+        }
+        if (!fileMissionList.canRead()) {
+            throw new IOException("Brak dostępu do pliku z listą misji.");
+        }
+        missionList = new ArrayList<>();
+        String line = "";
+        String splitBy = ";";
+        BufferedReader br = new BufferedReader(new FileReader(fileMissionList));
+        while ((line = br.readLine()) != null) {
+            String[] missionDetails = line.split(splitBy);
+            missionList.add(new Mission(missionDetails[0], missionDetails[1], missionDetails[2], Boolean.parseBoolean(missionDetails[3])));
         }
     }
 
-    int addRecord(Mission mission) {
-        try {
-
-            FileWriter writer = new FileWriter(fileMissionList, true);
-            //writer.append(missionName + ";" + budgetString + ";" + priority);
-            System.out.println(mission.toString());
+    void addRecord(Mission mission) {
+        try (FileWriter writer = new FileWriter(fileMissionList, true)) {
+            missionList.add(mission);
             writer.write(mission.toString());
-            writer.close();
             listMissions();
-            return 0;
         } catch (IOException e) {
             System.out.println("Blad inicjalizacji zapisu do pliku!");
-            return -1;
         }
     }
 
     void listMissions() {
-        String line = "";
-        String splitBy = ";";
-        try {
-
-            BufferedReader br = new BufferedReader(new FileReader(fileMissionList));
-            while ((line = br.readLine()) != null)   //returns a Boolean value
-            {
-                String[] missionDetails = line.split(splitBy);    // use comma as separator
-                System.out.println("Details: [Name=" + missionDetails[0] + ", Cost=" + missionDetails[1] + ", Priority=" + missionDetails[2] + ", IsBlacklisted=" + missionDetails[3] + "]");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (Mission mission : missionList) {
+            System.out.println("Details: [Name=" + mission.getMissionName() + ", Cost=" + mission.getBudgetString() + ", Priority=" + mission.getPriority() + ", IsBlacklisted=" + mission.isBlacklisted() + "]");
         }
     }
 /*
@@ -63,7 +54,5 @@ public class MissionListHandler {
         secPane.getChildren().add(newLoadedPane);
     }
 */
-    //TODO ta klasa będzie tylko obsługiwała plik ze wszystkimi klasmi
-    // do wyboru misji przez algotytm będzie nowa
 }
 
