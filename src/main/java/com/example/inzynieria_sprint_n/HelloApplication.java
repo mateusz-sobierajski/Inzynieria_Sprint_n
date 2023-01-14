1667ba449a5953e4d9c0f6b35dc2fb6c77019234
Mateusz Krzysiek <73441601+christaf@users.noreply.github.com>
2023-01-14 01:48:51 +0100

    Starting to cooperate with Login_screen.fxml First logins attempts

--- src/main/java/com/example/inzynieria_sprint_n/HelloApplication.java
+++ src/main/java/com/example/inzynieria_sprint_n/HelloApplication.java
@@ -2,10 +2,15 @@ package com.example.inzynieria_sprint_n;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
@@ -14,11 +19,30 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login_screen.fxml"));
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Budget_ass_screen.fxml"));
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Mission_mgmt.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("NASA");
-        stage.setScene(scene);
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Login_screen.fxml")));
+        stage.setTitle("N A S A");
+        stage.setScene(new Scene(root, 600, 400));
         stage.show();
+
+        TextField usernameField = (TextField) root.lookup("#usernameField");
+        PasswordField passwordField = (PasswordField) root.lookup("#passwordField");
+        Button loginButton = (Button) root.lookup("#loginButton");
+
+        loginButton.setOnAction(event -> {
+            String username = usernameField.getText();
+            String password = passwordField.getText();
+
+            // Perform validation on the username and password
+            if (username.isEmpty() || password.isEmpty()) {
+                // Show an error message if the fields are empty
+                System.out.println("Please enter a username and password.");
+            } else {
+                System.out.println("Attempting to log in with username: " + username + " and password: " + password);
+            }
+        });
     }
+
 
     public static void main(String[] args) {
         launch();
