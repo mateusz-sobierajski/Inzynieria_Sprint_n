cff75c33bba6a541d53f459803739c9b68deef2c
Mateusz Krzysiek <73441601+christaf@users.noreply.github.com>
2023-01-17 15:46:47 +0100

    singleton MissionDistributor

--- src/main/java/com/example/inzynieria_sprint_n/MissionDistributor.java
+++ src/main/java/com/example/inzynieria_sprint_n/MissionDistributor.java
@@ -5,87 +5,43 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 
-class Product {
-    private String name;
-    private int price;
-    private int cost;
-    private String permission;
-
-    public Product(String name, int price, int weight, String permission) {
-        this.name = name;
-        this.price = price;
-        this.cost = weight;
-        this.permission = permission;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPriority() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getCost() {
-        return cost;
-    }
-
-    public void setCost(int cost) {
-        this.cost = cost;
-    }
-
-
-    public String getPermission() {
-        return permission;
-    }
-
-    public void setPermission(String permission) {
-        this.permission = permission;
-    }
-}
-
 public class MissionDistributor {
-    public static void main(String[] args) throws IOException {
+
+    private static MissionDistributor missiondistributor;
+    public void chooseMissions() throws IOException {
         // Wczytanie pliku CSV
-        FileReader fileReader = new FileReader("src/main/java/com/example/inzynieria_sprint_n/csv/items.csv");
-        CSVParser csvParser = new CSVParser(fileReader, CSVFormat.DEFAULT);
+        FileReader fileReader = new FileReader("src/main/java/com/example/inzynieria_sprint_n/csv/proposed_mission_list.csv");
+        CSVParser csvParser = new CSVParser(fileReader, CSVFormat.DEFAULT.withDelimiter(';').withIgnoreEmptyLines(true));
         List<CSVRecord> csvRecords = csvParser.getRecords();
         csvParser.close();
 
         // Stworzenie listy obiektów na podstawie danych z pliku CSV
         List<Mission> products = new ArrayList<>();
         for (CSVRecord csvRecord : csvRecords) {
-//pobieranie kosztow z 2 kolumny
-            int priority = Integer.parseInt(csvRecord.get(2)); //pobieranie priorytetu z 3 kolumny
-            Mission mission = new Mission(csvRecord.get(0), csvRecord.get(1),csvRecord.get(2), Boolean.getBoolean(csvRecord.get(3)));
-            //if(csvRecord.get(3) == "false")
-            products.add(mission);
+            Mission mission = new Mission(csvRecord.get(0), csvRecord.get(1), csvRecord.get(2), Boolean.getBoolean(csvRecord.get(3)));
+            if (!mission.isBlacklisted())
+                products.add(mission);
         }
 
         // Użycie algorytmu plecakowego do wyboru odpowiednich produktów
-        int capacity = 22; // zmienić w zaleznosci od budzetu
+        int capacity = 500; // zmienić w zaleznosci od budzetu
         List<Mission> selectedProducts = knapsack(products, capacity);
 
+        /*
+        System.out.println("----------------------");
         for (Mission mission : selectedProducts) {
             System.out.println("Name: " + mission.getMissionName());
             System.out.println("Cost: " + mission.getBudget());
             System.out.println("Priority: " + mission.getPriority());
-            System.out.println("Permission: " + mission.isBlacklisted());
             System.out.println("----------------------");
         }
+         */
 
 
         // Zapis wybranych produktów do nowego pliku CSV
@@ -108,7 +64,7 @@ public class MissionDistributor {
                 if (mission.getBudget() > j) {
                     dp[i][j] = dp[i - 1][j];
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - (int)mission.getBudget()] + mission.getPriority());
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - (int) mission.getBudget()] + mission.getPriority());
                 }
             }
         }
