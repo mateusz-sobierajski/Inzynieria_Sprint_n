5d8d58a5542da9b052d2c44989cd4fda8fd7c3f6
mateusz-sobierajski <mateusz.sobierajski.official@gmail.com>
2023-01-17 10:51:41 +0100

    Dodatkowe komentarze javadoc, przerobienie BudgetManager na singleton

--- src/main/java/com/example/inzynieria_sprint_n/BudgetManager.java
+++ src/main/java/com/example/inzynieria_sprint_n/BudgetManager.java
@@ -6,14 +6,46 @@ import java.net.URL;
 import java.nio.file.Paths;
 import java.util.Objects;
 
+/**
+ * klasa BudgetManager jest singletonem posiadającym i przechowującym budżet całej agencji, którego wartość odczytuje z pliku csv
+ */
 public class BudgetManager {
     private long currentAgencyBudget;
     private final File budgetFile;
 
+    private static BudgetManager budgetmanager;
+
+    static {
+        try {
+            budgetmanager = new BudgetManager();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Funkcja getInstance zwraca instancję singletonu BudgetManager
+     * @return BudgetManager
+     */
+    public static BudgetManager getInstance(){
+        return budgetmanager;
+    }
+
+    /**
+     * Funkcja getCurrentBUdget zwraca budżet agencji jako wartość long
+     * @return currentAgencyBudget
+     */
     public long getCurrentBudget() {
         return currentAgencyBudget;
     }
 
+    /**
+     * Funkcjia setCurrentAgencyBudget pozwala na zmianę wartości budżetu zapisanego w instancji klasy i pliku csv
+     * @param newBudget
+     * @throws IOException
+     */
     public void setCurrentAgencyBudget(long newBudget) throws IOException {
         currentAgencyBudget = newBudget;
         if (!budgetFile.canWrite()) {
@@ -24,7 +56,12 @@ public class BudgetManager {
         }
     }
 
-    public BudgetManager() throws IOException, URISyntaxException {
+    /**
+     * Konstruktor klasy BudgetManager tworzy instancję klasy BudgetManager na podstawie zapisanych w pliku pliku csv
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    private BudgetManager() throws IOException, URISyntaxException {
 
         URL fileUrl = getClass().getResource("/csv/budget_file.csv");
         File file = Paths.get(Objects.requireNonNull(fileUrl).toURI()).toFile();
@@ -36,6 +73,9 @@ public class BudgetManager {
                 throw new IOException("Plik z budżetem jest pusty.");
             }
             currentAgencyBudget = Long.parseLong(currentLine);
+        }
+        catch (Exception e){
+            System.out.println(e);
         }
     }
 }
