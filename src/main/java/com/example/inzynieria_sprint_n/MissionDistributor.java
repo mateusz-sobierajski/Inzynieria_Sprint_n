86848691a2f315143c20b767bbac2202f6f98aa0
Mateusz Krzysiek <73441601+christaf@users.noreply.github.com>
2023-01-29 21:11:02 +0100

    added algorithm knapsackNoPriority

--- src/main/java/com/example/inzynieria_sprint_n/MissionDistributor.java
+++ src/main/java/com/example/inzynieria_sprint_n/MissionDistributor.java
@@ -86,4 +86,35 @@ public class MissionDistributor {
 
         return selectedProducts;
     }
+    public static List<Mission> knapsackNoPriority(List<Mission> products, int capacity) {
+        // Create a 2D array to store values
+        int[][] dp = new int[products.size() + 1][capacity + 1];
+
+        // Fill the array with values
+        for (int i = 1; i <= products.size(); i++) {
+            Mission mission = products.get(i - 1);
+            for (int j = 1; j <= capacity; j++) {
+                if (mission.getBudget() > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - (int) mission.getBudget()] + 1);
+                }
+            }
+        }
+
+        // Reconstruct the solution (choose products)
+        List<Mission> selectedProducts = new ArrayList<>();
+        int i = products.size();
+        int j = capacity;
+        while (i > 0 && j > 0) {
+            if (dp[i][j] != dp[i - 1][j]) {
+                selectedProducts.add(0, products.get(i - 1));
+                j -= products.get(i - 1).getBudget();
+            }
+            i--;
+        }
+
+        return selectedProducts;
+    }
+
 }
